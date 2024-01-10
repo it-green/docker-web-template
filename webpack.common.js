030c3c7fc68b2e94fb898276465a008f42c5b514
it-green <ryo.ctuad@gmail.com>
2024-01-10 16:48:51 +0900

    update: nginx

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,14 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
+// 開発ファイルへのパス
+const filePath = {
+  scss: './www/src/assets/scss/',
+  js: './www/src/assets/js/'
+}
 
 module.exports = () => ({
 
