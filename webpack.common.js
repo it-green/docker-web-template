6190aef1db61a133f3b8a465bee8c334bc64ca33
it-green <ryo.ctuad@gmail.com>
2024-01-10 18:19:29 +0900

    format files

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,10 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin')
 
-// 開発ファイルへのパス
-const filePath = {
-  scss: './www/src/assets/scss/',
-  js: './www/src/assets/js/'
-}
-
-module.exports = () => ({
-
-});
+module.exports = {}
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,28 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
+const { merge } = require('webpack-merge')
+const path = require('path')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
-const { htmlWebpackPluginTemplateCustomizer } = require('template-ejs-loader');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin')
+const { htmlWebpackPluginTemplateCustomizer } = require('template-ejs-loader')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
-const common = require('./webpack.common');
+const common = require('./webpack.common')
 
 // 開発ファイルへのパス
 const filePath = {
   ejs: './www/src/pages/',
-  images: './www/src/assets/images/'
+  images: './www/src/assets/images/',
 }
 
 // EJSの読み込み
-const ejsEntries = WebpackWatchedGlobEntries.getEntries([path.resolve(__dirname, `${filePath.ejs}**/*.ejs`)], {
-  ignore: path.relative(__dirname, `${filePath.ejs}**/_*.ejs`),
-})();
+const ejsEntries = WebpackWatchedGlobEntries.getEntries(
+  [path.resolve(__dirname, `${filePath.ejs}**/*.ejs`)],
+  {
+    ignore: path.relative(__dirname, `${filePath.ejs}**/_*.ejs`),
+  }
+)()
 const htmlGlobPlugins = (entries) => {
   return Object.keys(entries).map(
     (key) =>
@@ -40,18 +43,20 @@ const htmlGlobPlugins = (entries) => {
           collapseWhitespace: true,
           preserveLineBreaks: true,
         },
-      }),
-  );
-};
+      })
+  )
+}
 
 // webpに変換
 const imgWebpPlugin = () => {
   return [
     new CopyWebpackPlugin({
-      patterns: [{
-        from: path.resolve(__dirname, 'www/src/assets/images'),
-        to: path.resolve(__dirname, 'www/src/dest/assets/images'),
-      }],
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'www/src/assets/images'),
+          to: path.resolve(__dirname, 'www/src/dest/assets/images'),
+        },
+      ],
     }),
     new ImageMinimizerPlugin({
       test: /\.(jpe?g|png)$/i,
@@ -60,9 +65,7 @@ const imgWebpPlugin = () => {
           type: 'asset',
           implementation: ImageMinimizerPlugin.imageminGenerate,
           options: {
-            plugins: [
-              ['webp', { quality: 80 }],
-            ],
+            plugins: [['webp', { quality: 80 }]],
           },
         },
       ],
@@ -75,9 +78,7 @@ const imgWebpPlugin = () => {
         options: {
           encodeOptions: {
             multipass: true,
-            plugins: [
-              'preset-default',
-            ],
+            plugins: ['preset-default'],
           },
         },
       },
@@ -88,14 +89,12 @@ const imgWebpPlugin = () => {
       minimizer: {
         implementation: ImageMinimizerPlugin.imageminMinify,
         options: {
-          plugins: [
-            ["gifsicle", { interlaced: true }],
-          ],
+          plugins: [['gifsicle', { interlaced: true }]],
         },
       },
     }),
-  ];
-};
+  ]
+}
 
 const browserSync = () => {
   return [
@@ -107,7 +106,13 @@ const browserSync = () => {
         target: 'http://localhost:8080',
       },
       watchOptions: {
-        ignored: ['webpack.config.js', 'node_modules', 'package.json', 'package-lock.json', 'readme.md'],
+        ignored: [
+          'webpack.config.js',
+          'node_modules',
+          'package.json',
+          'package-lock.json',
+          'readme.md',
+        ],
       },
       open: true,
       ghostMode: {
@@ -117,7 +122,7 @@ const browserSync = () => {
       },
       logLevel: 'debug',
     }),
-  ];
+  ]
 }
 
 const app = {
@@ -134,7 +139,7 @@ const app = {
         test: /\.ejs$/i,
         use: ['html-loader', 'template-ejs-loader'],
       },
-    ]
+    ],
   },
   plugins: [
     ...htmlGlobPlugins(ejsEntries),
@@ -143,4 +148,4 @@ const app = {
   ],
 }
 
-module.exports = merge(common(), app);
+module.exports = merge(common, app)
